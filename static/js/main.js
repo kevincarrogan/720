5b473070dd3f5292edcdb8de9be7c4dd847f65d3
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-12-20 21:43:51 +0000

    Making the initial clock larger.

--- static/js/main.js
+++ static/js/main.js
@@ -5,16 +5,20 @@ var Clock = function (two, hour, minute) {
 };
 
 Clock.prototype.render = function () {
-    var outer = this.two.makeCircle(100, 100, 50);
+    var outer = this.two.makeCircle(250, 250, 200);
 
-    var inner = this.two.makeCircle(100, 100, 45);
+    var inner = this.two.makeCircle(250, 250, 180);
     inner.fill = '#1f1f1f';
 
-    var hourHand = this.two.makeLine(100, 100, 70, 100);
+    var hourHand = this.two.makeLine(250, 250, 250, 100);
     hourHand.stroke = '#fff';
+    hourHand.linewidth = 10;
+    hourHand.cap = 'round';
 
-    var minuteHand = this.two.makeLine(100, 100, 100, 60);
+    var minuteHand = this.two.makeLine(250, 250, 150, 250);
     minuteHand.stroke = '#fff';
+    minuteHand.linewidth = 10;
+    minuteHand.cap = 'round';
 };
 
 var Scene = function (el) {
