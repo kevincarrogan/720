3d940420e8a1f1986566b8015b6fb5bf473e2bfc
Kevin Carrogan <kevin.carrogan@gmail.com>
2014-12-20 22:49:08 +0000

    Fixing hour hand and minute hand.

--- static/js/main.js
+++ static/js/main.js
@@ -10,15 +10,15 @@ Clock.prototype.render = function () {
     var inner = this.two.makeCircle(250, 250, 180);
     inner.fill = '#1f1f1f';
 
-    var hourHand = this.two.makeLine(250, 250, 250, 100);
-    hourHand.stroke = '#fff';
-    hourHand.linewidth = 10;
-    hourHand.cap = 'round';
-
-    var minuteHand = this.two.makeLine(250, 250, 150, 250);
+    var minuteHand = this.two.makeLine(250, 250, 250, 100);
     minuteHand.stroke = '#fff';
     minuteHand.linewidth = 10;
     minuteHand.cap = 'round';
+
+    var hourHand = this.two.makeLine(250, 250, 150, 250);
+    hourHand.stroke = '#fff';
+    hourHand.linewidth = 10;
+    hourHand.cap = 'round';
 };
 
 var Scene = function (el) {
